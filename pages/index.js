f7509be9e9076cf3471c3a497e3a5c93fde3ff1b
Gaj Mohan <gaj@matchpint.co.uk>
2021-03-14 14:36:41 +0000

    simple setup

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,66 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
+  const links = [
+    {
+      type: "twitter",
+      name: "@inspectorgajit",
+      url: "https://twitter.com/Inspectorgajit",
+    },
+    {
+      type: "linkedin",
+      name: "Inspectorgajit",
+      url: "https://twitter.com/Inspectorgajit",
+    },
+    {
+      type: "github",
+      name: "Github",
+      url: "https://twitter.com/Inspectorgajit",
+    },
+  ];
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <section class="text-gray-700 body-font">
+      <div class="container flex flex-col items-center px-5 py-16 mx-auto lg:px-20 lg:py-24 md:flex-row">
+        <div class="w-5/6 mb-10 lg:max-w-lg lg:w-full md:w-1/2 md:mb-0">
+          <img
+            class="object-cover object-center rounded"
+            alt="hero"
+            src="/me.jpg"
+          />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        <div class="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
+          <div class="flex flex-wrap -mx-4 -mt-4 -mb-10 sm:-m-4 ">
+            <div class="container flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow">
+              <ul class="flex flex-col divide divide-y">
+                {links.map((link) => {
+                  return (
+                    <Link type={link.type} name={link.name} url={link.url} />
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
+
+const Link = ({ type, name, url }) => {
+  return (
+    <li class="flex flex-row">
+      <a href={url} class="block relative" target="_blank">
+        <div class="select-none cursor-pointer flex flex-1 items-center p-4">
+          <div class="flex flex-col w-10 h-10 justify-center items-center mr-4">
+            <img
+              alt="profil"
+              src={"/icons/" + type + ".svg"}
+              class="mx-auto object-cover rounded-full h-10 w-10 "
+            />
+          </div>
+          <div class="flex-1 pl-1 mr-16">
+            <div class="font-medium dark:text-white">{name}</div>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+};

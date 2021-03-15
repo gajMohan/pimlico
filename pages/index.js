5a4a9d0bae8b475770a0beca73d895101b1d49fc
Gaj Mohan <gaj@matchpint.co.uk>
2021-03-15 19:28:11 +0000

    dynamic image

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,36 @@
 import Head from "next/head";
 
 export default function Home(props) {
-  console.log(props);
   return (
-    <section class="text-gray-700 body-font">
+    <section className="text-gray-700 body-font">
       <Head>
         <title>{props.personal.name.short}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-HKMRNQH6G5"
-        ></script>
       </Head>
-      <div class="container flex flex-col items-center px-5 py-16 mx-auto lg:px-20 lg:py-24 md:flex-row">
-        <div class="w-5/6 mb-10 lg:max-w-lg lg:w-full md:w-1/2 md:mb-0">
+      <div className="container flex flex-col items-center px-5 py-16 mx-auto lg:px-20 lg:py-24 md:flex-row">
+        <div className="w-5/6 mb-10 lg:max-w-lg lg:w-full md:w-1/2 md:mb-0">
           <img
-            class="object-cover object-center rounded"
+            className="object-cover object-center rounded"
             alt="hero"
-            src="/me.jpg"
+            src={props.personal.image}
           />
         </div>
-        <div class="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
-          <div class="">
-            <div class="text-center">
-              <p class="text-2xl text-gray-800 dark:text-white">
+        <div className="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
+          <div className="">
+            <div className="text-center">
+              <p className="text-2xl text-gray-800 dark:text-white">
                 {props.personal.name.short}
               </p>
-              <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+              <p className="text-xl text-gray-500 dark:text-gray-200 font-light">
                 {props.personal.location}
               </p>
-              <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+              <p className="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
                 {props.personal.description}
               </p>
             </div>
-            <div class="pt-8 flex border-t border-gray-200 w-44 mx-auto text-gray-500 items-center justify-between">
+            <div className="pt-8 flex border-t border-gray-200 w-44 mx-auto text-gray-500 items-center justify-between">
               {props.personal.links.map((link) => {
-                return <Link type={link.type} url={link.url} />;
+                return <Link key={link.type} type={link.type} url={link.url} />;
               })}
             </div>
           </div>
@@ -51,7 +46,7 @@ const Link = ({ type, url }) => {
       <img
         alt="profil"
         src={"/icons/" + type + ".svg"}
-        class="mx-auto object-cover rounded-full h-10 w-10 "
+        className="mx-auto object-cover rounded-full h-10 w-10 "
       />
     </a>
   );
